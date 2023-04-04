4f927cba0f536869cc81e8fadd48b5d90cfb3702
bey8wie3pe <94910229+bey8wie3pe@users.noreply.github.com>
2023-04-04 16:03:09 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mysql = require('mysql');
 const ejs = require('ejs');
-
+const session = require('express-session');
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// MySQL接続設定
+//MySQL接続設定
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -15,21 +15,47 @@ const connection = mysql.createConnection({
   database: 'todo_app'
 });
 
-// DB接続
+//DB接続
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected to MySQL');
 });
 
-// タスク一覧ページ
+//ログインセッション
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: true,
+	// HTTPSでない場合はfalseにする必要がある
+  cookie: { secure: false }
+}));
+
+//タスクページ
 app.get('/', (req, res) => {
+	//ログインしていないときリダイレクトする
+  if (!req.session.userId) {
+    return res.redirect('/login');
+  }
+
+	//すべてのタスクを取得
   connection.query('SELECT * FROM tasks', (err, results) => {
     if (err) throw err;
     res.render('index', { tasks: results });
   });
 });
 
-// タスク追加処理
+
+//アカウント作成ページ
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+//ログインページ表示
+app.get('/login', (req, res) => {
+  res.render('login', {errorMessage: null});
+});
+
+//タスク追加処理
 app.post('/add', (req, res) => {
   const taskName = req.body.taskName;
   connection.query('INSERT INTO tasks (task_name) VALUES (?)', [taskName], (err, results) => {
@@ -46,6 +72,40 @@ app.post('/delete', (req, res) => {
     res.redirect('/');
   });
 });
+
+// アカウント作成処理
+app.post('/signup', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  connection.query('SELECT * FROM users WHERE user_name = ?', [username], (err, results) => {
+    if (err) throw err;
+
+    if (results.length > 0) {
+      res.render('signup', { errorMessage: 'ユーザー名が既に使用されています' });
+    } else {
+      connection.query('INSERT INTO users (user_name, user_password) VALUES (?, ?)', [username, password], (err, results) => {
+        if (err) throw err;
+        res.redirect('/login');
+      });
+    }
+  });
+});
+
+// ログイン処理
+app.post('/login', (req, res) => {
+const username = req.body.username;
+const password = req.body.password;
+connection.query('SELECT * FROM users WHERE user_name = ? AND user_password = ?', [username, password], (err, results) => {
+  if (results.length > 0) {
+    req.session.userId = results[0].id;
+    res.redirect('/');
+  } else {
+    res.send('ユーザーネームかパスワードが違います');
+  }
+});
+});
+
 
 // サーバー起動
 app.listen(3000, () => {
